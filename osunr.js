7b1ee2fcdbf31b639effb1247379b6cad347aa01
Guido Pezzotta <134343706+GuiduusRC98@users.noreply.github.com>
2023-09-19 09:29:29 -0300

    prueba de interes

--- osunr.js
+++ osunr.js
@@ -1,24 +1,50 @@
-const calcularInteres = () => {
-    const genero = document.getElementById('genero').value;
-    const edad = parseInt(document.getElementById('edad').value);
-    const dias = parseInt(document.getElementById('dias').value);
-    const cuotas = parseInt(document.getElementById('cuotas').value);
-    const importe = parseFloat(document.getElementById('importe').value);
-    
-    const edadMujer = 60;
-    const edadHombre = 65;
-  
-    if (genero === "hombre" && edad < edadHombre) {
-      const resultadoFinal = (2.5 / 30) * (30 - dias) + (2.5 * cuotas);
-      const interes = (resultadoFinal * importe) / 100;
-      const total = importe + interes;
-      document.getElementById('resultado').innerHTML = `El interés es: ${interes.toFixed(2)}<br>Total a pagar: ${total.toFixed(2)}`;
-    } else if (genero === "mujer" && edad < edadMujer) {
-      const resultadoFinal = (2.5 / 30) * (30 - dias) + (2.5 * cuotas);
-      const interes = (resultadoFinal * importe) / 100;
-      const total = importe + interes;
-      document.getElementById('resultado').innerHTML = `El interés es: ${interes.toFixed(2)}<br>Total a pagar: ${total.toFixed(2)}`;
+class CalculadoraInteres {
+  constructor() {
+    this.genero = document.getElementById('genero').value;
+    this.edad = parseInt(document.getElementById('edad').value);
+    this.dias = parseInt(document.getElementById('dias').value);
+    this.cuotas = parseInt(document.getElementById('cuotas').value);
+    this.importe = parseFloat(document.getElementById('importe').value);
+  }
+
+  calcularInteres() {
+    let resultadoFinal = 0;
+    let interes = 0;
+    let total = 0;
+
+    if (this.genero === "hombre" && this.edad < edadHombre) {
+      resultadoFinal = (this.cuotas / 30) * (30 - this.dias) + this.calcularTotalInteres();
+      interes = (resultadoFinal * this.importe) / 100;
+      total = this.importe + interes;
+      this.mostrarResultado(interes, total);
+    } else if (this.genero === "mujer" && this.edad < edadMujer) {
+      resultadoFinal = (2.5 / 30) * (30 - this.dias) + this.calcularTotalInteres();
+      interes = (resultadoFinal * this.importe) / 100;
+      total = this.importe + interes;
+      this.mostrarResultado(interes, total);
     } else {
-      document.getElementById('resultado').innerHTML = "No puedes realizar financiación.";
+      this.mostrarResultado("No puedes realizar financiación.");
     }
-  };
+  }
+
+  calcularTotalInteres() {
+    return this.cuotas * this.cuotas;
+  }
+
+  mostrarResultado(interes, total) {
+    const resultadoElement = document.getElementById('resultado');
+    resultadoElement.innerHTML = `El interés es: ${interes.toFixed(2)}<br>Total a pagar: ${total.toFixed(2)}`;
+  }
+}
+
+// Uso de la clase
+const edadHombre = 65 // Debes definir esta variable
+const edadMujer = 60 // Debes definir esta variable
+
+const calculadora = new CalculadoraInteres();
+calculadora.calcularInteres();
+
+
+
+  
+
