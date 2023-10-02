a9da0cae46e256b96f244f891d5a834bde14e6b6
Guido Pezzotta <134343706+GuiduusRC98@users.noreply.github.com>
2023-10-02 10:46:03 -0300

    ACTUALIZADO A 9 cuotas

--- osunr.js
+++ osunr.js
@@ -8,16 +8,17 @@ const calcularInteres = () => {
 
   
     if (genero === "hombre" && edad < edadHombre) {
-      const resultadoFinal = (cuotas / 30) * (30 - dias) + (totalInteres);
-      const totalInteres = (cuotas * cuotas)
+      const resultadoFinal = (9 / 30) * (30 - dias) + (9 * cuotas);
       const interes = (resultadoFinal * importe) / 100;
       const total = importe + interes;
-      document.getElementById('resultado').innerHTML = `El interés es: ${interes.toFixed(2)}<br>Total a pagar: ${total.toFixed(2)}`;
+      const cadaCuota = total / cuotas;
+      document.getElementById('resultado').innerHTML = `El interés es: ${interes.toFixed(2)}\n<br>Total a pagar: ${cadaCuota.toFixed(2)}\n<br>Total a pagar: ${total.toFixed(2)}`;
     } else if (genero === "mujer" && edad < edadMujer) {
-      const resultadoFinal = (2.5 / 30) * (30 - dias) + (totalInteres);
+      const resultadoFinal = (9 / 30) * (30 - dias) + (9 * cuotas);
       const interes = (resultadoFinal * importe) / 100;
       const total = importe + interes;
-      document.getElementById('resultado').innerHTML = `El interés es: ${interes.toFixed(2)}<br>Total a pagar: ${total.toFixed(2)}`;
+      const cadaCuota = total / cuotas;
+      document.getElementById('resultado').innerHTML = `El interés es: ${interes.toFixed(2)}\n<br>Total a pagar: ${cadaCuota.toFixed(2)}\n<br>Total a pagar: ${total.toFixed(2)}`;
     } else {
       document.getElementById('resultado').innerHTML = "No puedes realizar financiación.";
     }
