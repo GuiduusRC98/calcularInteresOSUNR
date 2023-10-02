0aea1deaaf9e9290ccae44e1bdbd36de9201e7ef
Guido Pezzotta <134343706+GuiduusRC98@users.noreply.github.com>
2023-10-02 13:36:10 -0300

    aaaa

--- osunr.js
+++ osunr.js
@@ -14,16 +14,16 @@ const calcularInteres = () => {
       const total = importe + interes;
       const cadaCuota = total / cuotas;
       document.getElementById('resultado').innerHTML = `${total.toFixed(2)}`;
-      document.getElementById('intereses').innerHTML = `${interes.toFixed(2)}`
-      document.getElementById('cuotas').innerHTML = `${cadaCuota.toFixed(2)}`
+      document.getElementById('intereses').innerHTML = `${interes.toFixed(2)}`;
+      document.getElementById('cuotas').innerHTML = `${cadaCuota.toFixed(2)}`;
     } else if (genero === "mujer" && edad < edadMujer) {
       const resultadoFinal = (9 / 30) * (30 - dias) + (9 * cuotas);
       const interes = (resultadoFinal * importe) / 100;
       const total = importe + interes;
       const cadaCuota = total / cuotas;
       document.getElementById('resultado').innerHTML = `${total.toFixed(2)}`;
-      document.getElementById('intereses').innerHTML = `${interes.toFixed(2)}`
-      document.getElementById('cuotas').innerHTML = `${cadaCuota.toFixed(2)}`
+      document.getElementById('intereses').innerHTML = `${interes.toFixed(2)}`;
+      document.getElementById('cuotas').innerHTML = `${cadaCuota.toFixed(2)}`;
     } else {
       document.getElementById('resultado').innerHTML = "No puedes realizar financiación.";
     }
