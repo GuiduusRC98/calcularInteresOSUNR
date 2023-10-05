f73f9e548edebda3847acb7775b8cc4c4af31288
Guido Pezzotta <134343706+GuiduusRC98@users.noreply.github.com>
2023-10-05 12:30:41 -0300

    areglado

--- osunr.js
+++ osunr.js
@@ -14,7 +14,7 @@ const calcularInteres = () => {
       const total = importe + interes;
       const cadaCuota = total / cuotas;
       const interesMensual = resultadoFinal / cuotas;
-      document.getElementById('resultado').innerHTML = `El Total es:<strong>${total.toFixed(2)}</strong><hr><br>El interés total es:<strong>${interes.toFixed(2)}</strong><<hr>br>Cada cuota es:<strong>${cadaCuota.toFixed(2)}</strong><hr><br>Interes total:<strong>${resultadoFinal.toFixed(2)}</strong><hr><br>Interes mensual:<strong>${interesMensual.toFixed(2)}</strong>`;
+      document.getElementById('resultado').innerHTML = `El Total es:<strong>${total.toFixed(2)}</strong><hr><br>El interés total es:<strong>${interes.toFixed(2)}</strong><hr><br>Cada cuota es:<strong>${cadaCuota.toFixed(2)}</strong><hr><br>Interes total:<strong>${resultadoFinal.toFixed(2)}</strong><hr><br>Interes mensual:<strong>${interesMensual.toFixed(2)}</strong>`;
     } else if (genero === "mujer" && edad < edadMujer) {
       const resultadoFinal = (9 / 30) * (30 - dias) + (9 * cuotas);
       const interes = (resultadoFinal * importe) / 100;
